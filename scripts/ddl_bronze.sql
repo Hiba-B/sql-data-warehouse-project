fdb655515ade44bcf7b04fff6d15875a0d2a8965
Hiba-B <58844847+Hiba-B@users.noreply.github.com>
2025-10-15 09:26:37 +0200

    ddl_bronze.sql

--- scripts/ddl_bronze.sql
+++ scripts/ddl_bronze.sql
@@ -1,3 +1,12 @@
+/*
+
+This SQL script sets up staging tables in the `bronze` schema by dropping them if they exist and recreating them with defined structures. 
+It includes tables for CRM customer info, product details, and sales transactions, as well as ERP customer demographics, location data, and product category metadata. 
+These tables serve as raw data ingestion points in a data pipeline, likely used for further transformation and analysis.
+
+
+*/
+
 IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
 
@@ -72,114 +81,3 @@ create table bronze.erp_px_cat_g1v2 (
 	maintenance NVARCHAR(50)
 
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
-BEGIN
-	DECLARE @start_time DATETIME,
-	@end_time DATETIME,
-	@batch_start_time DATETIME,
-	@batch_end_time DATETIME;
-	BEGIN TRY
-	  SET @batch_start_time = GETDATE();
-		PRINT '=======================================' 
-		PRINT 'Loading Bronze Layer'
-		PRINT '=======================================' 
-		PRINT '-----------------'
-
-		SET @start_time = GETDATE();
-		PRINT 'Loading CRM Tables'
-		PRINT '-----------------'
-		TRUNCATE TABLE bronze.crm_cust_info;
-		BULK INSERT bronze.crm_cust_info
-		FROM 'D:\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-
-		TRUNCATE TABLE bronze.crm_prd_info;
-		BULK INSERT bronze.crm_prd_info
-		FROM 'D:\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-
-		TRUNCATE TABLE bronze.crm_sales_details;
-		BULK INSERT bronze.crm_sales_details
-		FROM 'D:\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-		SET @end_time = GETDATE();
-		PRINT '>>>>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds'; 
-		PRINT('>>> ---------')
-		SET @start_time = GETDATE();
-			PRINT '-----------------'
-		PRINT 'Loading ERP Tables'
-		PRINT '-----------------'
-		TRUNCATE TABLE bronze.erp_cust_az12;
-		BULK INSERT bronze.erp_cust_az12
-		FROM 'D:\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-
-		TRUNCATE TABLE bronze.erp_loc_a101;
-		BULK INSERT bronze.erp_loc_a101
-		FROM 'D:\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-
-		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'D:\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-		SET @end_time = GETDATE();
-		PRINT '>>>>> Load Duration:' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds'; 
-		PRINT('>>> ---------')
-
-			SET @batch_end_time = GETDATE();
-	PRINT '================================'
-	PRINT'Bronzer Layer is completed'
-	PRINT 'TOTAL DURATION :' + CAST( DATEDIFF(second, @batch_start_time, @batch_end_time)	as	nvarchar ) + ' seconds';
-	PRINT '================================'
-
-
-	END TRY 
-	BEGIN CATCH
-		PRINT '----------------'
-		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
-		PRINT 'ERROR MESSAGE' + ERROR_MESSAGE();
-		PRINT 'ERROR MESSAGE' + CAST (ERROR_NUMBER() AS NVARCHAR);
-		PRINT 'ERROR MESSAGE' + CAST (ERROR_STATE() AS NVARCHAR);
-		PRINT '----------------'
-
-	END CATCH
-END
